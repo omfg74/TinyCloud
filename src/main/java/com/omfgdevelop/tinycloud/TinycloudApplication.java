22203f1a0dc562beb2fda9547438e4dc6886d04d
omfg <podsh74@gmail.com>
2021-11-15 14:18:44 +0500

    First

---
+++ src/main/java/com/omfgdevelop/tinycloud/TinycloudApplication.java
@@ -0,0 +1,13 @@
+package com.omfgdevelop.tinycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinycloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinycloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omfgdevelop/tinycloud/TinycloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omfgdevelop.tinycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinycloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
